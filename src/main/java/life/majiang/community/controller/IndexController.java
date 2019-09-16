d6c911f16ac031a39451762ac20f4486daac0e16
wuzhangen <1572432185@qq.com>
2019-09-16 18:56:10 +0800

    third

--- src/main/java/life/majiang/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/controller/IndexController.java
@@ -1,4 +1,4 @@
-package life.majiang.community.community.controller;
+package life.majiang.community.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
